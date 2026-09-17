8525911675b0c5e7aaa49cb23191e1e21b688578
dawid1110 <dwiad1110@gmail.com>
2026-09-17 12:12:26 +0000

    Zmień typ wyjątku w obsłudze JWT na InvalidKeyTypeError

--- api.py
+++ api.py
@@ -51,7 +51,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         if user_id is None:
             raise HTTPException(status_code=401, detail="Nieprawidłowy token")
         return int(user_id)
-    except jwt.exceptions.InvalidTokenError:
+    except jwt.exceptions.InvalidKeyTypeError:
         raise HTTPException(status_code=401, detail="Nieprawidłowy lub wygasły token")
 
 # ==========================================
